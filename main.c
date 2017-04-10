666bfbea6801e220ad5b8896653c2e4a502bcaa8
Łukasz <lukasz.maksymilian.wolniak@gmail.com>
2017-04-10 18:13:45 +0300

    Temperature and Humidity
    services for temperature and humidity sensor

--- main.c
+++ main.c
@@ -1,9 +1,56 @@
+#include <stdio.h>
+#include "STM32F4xx.h"
+#include "dht11.h"
 
-int main(void)
-{
+// PATTERN: https://github.com/sapher/stm32-dht11_driver
 
-    while(1)
-    {
-    	//LUKASZ WOLNIAK
-    }
+struct DHT11_Dev dev;
+
+volatile uint32_t msTicks;
+
+void SysTick_Handler(void) {
+  msTicks++;
+}
+
+void Delay (uint32_t dlyTicks) {
+  uint32_t curTicks;
+
+  curTicks = msTicks;
+  while ((msTicks - curTicks) < dlyTicks);
+}
+
+int temparature = 0;
+int humidity = 0;
+
+int main (void) {
+
+	SystemInit();
+
+	SystemCoreClockUpdate();
+	if (SysTick_Config(SystemCoreClock / 1000)) {
+	while (1);
+	}
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+
+	DHT11_init(&dev, GPIOB, GPIO_Pin_6);
+
+	while(1) {
+		int res = DHT11_read(&dev);
+		if(res == DHT11_ERROR_CHECKSUM) {
+			//temporary - wolniak
+			temparature = 5500;
+			humidity = 200;
+		}
+		else if(res == DHT11_SUCCESS) {
+			temparature = dev.temparature; //0-50, +- 2 deegres
+			humidity = dev.humidity; //20-95%, +-RH
+		}
+		else {
+			//temporary - wolniak
+			temparature = -273;
+			humidity = -100;
+		}
+	}
 }
